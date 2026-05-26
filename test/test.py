e7a523f36ff612d50efcd48b4e6dd033dac0100a
Cambridge institute of technology k venkateswarlu <venkateswarlu.24ece@cambridge.edu.in>
2026-05-26 15:01:10 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,33 +8,27 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
+
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
+    # Start clock
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
+
     await ClockCycles(dut.clk, 10)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Running LFSR")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Wait some cycles
+    await ClockCycles(dut.clk, 20)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Check LFSR output is not stuck at zero
+    assert dut.uo_out.value != 0
